f0263778ed69ec1ea2b912627d3facf860b196a2
daivaj <34283132+daivaj@users.noreply.github.com>
2017-12-13 19:59:53 +0200

    pataisymai

--- index2.php
+++ index2.php
@@ -214,6 +214,35 @@ echo $y;
 echo '<br>';
 echo $z;
 
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+
+echo '<br>';
+
+$a = array(
+    [5, 6, 8, 9],
+    [12, 3, 4, 11],
+    [7, 5, 15, 10],
+
+);
+
+var_dump($a);
+
+
+
 ?>
 </body>
 </html>
