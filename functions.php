f883329d4d4957aa51da7a7fe6fc0d5af170dc0b
daivaj <34283132+daivaj@users.noreply.github.com>
2018-01-09 23:42:40 +0200

    duomenu bazes rusiavimas ir puslapiavimas

--- functions.php
+++ functions.php
@@ -1,44 +1,20 @@
 <?php
-function lentele($conn, $page, $offset) {
-// išvedame automobilius
-$sql = 'SELECT id, date, number, distance, time, distance/time*3.6 as speed FROM radars ORDER BY number, date DESC LIMIT ? OFFSET ?';
+function puslapiavimas($puslapis, $visoIrasu, $ip){
 
-$stmt = $conn->prepare($sql);
-$stmt->bind_param('ii', $page, $offset);
-$stmt->execute();
-
-/* bind result variables */
-$stmt->bind_result($id, $date, $number, $distance, $time, $speed);
-
-?>
-<table>
-    <tr>
-        <th>Nr.</th>
-        <th>id</th>
-        <th>Numeris</th>
-        <th>Data</th>
-        <th>Atstumas (km)</th>
-        <th>Laikas (h)</th>
-        <th>Greitis (km/h)</th>
-    </tr>
-
-    <?php
-    // output data of each row
-    $nr = 1; ?>
-    <?php  while($stmt->fetch()): ?>
-        <tr>
-            <td><?= $nr++ ?></td>
-            <td><?= $id ?></td>
-            <td><?= $number ?></td>
-            <td><?= $date ?></td>
-            <td><?= $distance ?></td>
-            <td><?= $time ?></td>
-            <td><?= $speed ?></td>
-        </tr>
-    <?php endwhile; ?>
-
-</table>
-<?php
+    $button = '';
+    if (!empty($_GET['button']))$button='&button='.$_GET['button'];
+    if ($puslapis > 0){
+        echo '<a href="?puslapis='.($puslapis-1).$button.'" >Atgal</a>';
+    }
+    $viso = ceil($visoIrasu/$ip);
+    for ($i = 0; $i < $viso; $i++){
+        $stilius = '';
+        if ($i == $puslapis){
+            $stilius = 'active';
+        }
+        echo '<a class="'.$stilius.'" href="?puslapis=' .($i).$button.'" >'.($i+1).'</a>';
+    }
+    if ($viso > $puslapis+1){
+        echo '<a href="?puslapis=' .($puslapis+1).$button.'" >Pirmyn</a>';
+    }
 }
-
-?>
--- index2.php
+++ index2.php
@@ -3,90 +3,194 @@
 <head>
     <title>Duomenu baze</title>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="reset.css">
+    <link rel="stylesheet" type="text/css" href="style.css?v=3">
 </head>
 <body>
 
 <?php
 require_once 'db.php';
-//
+require_once 'functions.php';
 
 $conn = connectDB();
 
-if (isset($_GET['delete'])) {
-    $sql = "DELETE FROM radars WHERE id = ". intval($_GET['delete']);
-    $conn->query($sql);
-}
-
-$row = [];
-if (isset($_GET['edit'])) {
-    $sql = "SELECT * FROM radars WHERE id = ". intval($_GET['edit']);
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-    }
-}
-
-if (isset($_POST['save'])) {
-    if (intval($_POST['id']) > 0) {
-        echo "update";
-        require_once 'update.php';
-    } else {
-        require_once 'insert.php';
-        echo "insert";
-    }
-}
-
 ?>
 
-<form method='post' action="?">
-    <input type='hidden' name='id' required value="<?php if (isset($row['id'])) echo $row['id'] ?>">
-    Data: <input type='text' name='date' required value="<?php if (isset($row['date'])) echo $row['date'] ?>"><br>
-    Numeris: <input type='text' name='number' required value="<?php if (isset($row['number'])) echo $row['number'] ?>"><br>
-    Atstumas: <input type='number' name='distance' required value="<?php if (isset($row['distance'])) echo $row['distance'] ?>"><br>
-    Laikas: <input type='number' name='time' required value="<?php if (isset($row['time'])) echo $row['time'] ?>">
-    <button name="save" type="submit">Išsaugoti</button>
+<form method='get' action="?">
+    <button name="button" value="auto" type="submit">Automobiliai</button>
+    <button name="button" value="year" type="submit">Metai</button>
+    <button name="button" value="month" type="submit">Menuo</button>
 </form>
 
-<hr>
-
 <?php
-// išvedame automobilius
-$sql = 'SELECT *, `distance`/`time`*3.6 as `speed` FROM radars ORDER BY number, date';
 
-$result = $conn->query($sql);
+$puslapis = @$_GET['puslapis'];
 
-if ($result->num_rows > 0) {
-    ?>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Numeris</th>
-            <th>Data</th>
-            <th>Atstumas (km)</th>
-            <th>Laikas (h)</th>
-            <th>Greitis (km/h)</th>
-            <th>Veiksmai</th>
-        </tr>
+$ip = 3;
 
-        <?php while($row = $result->fetch_assoc()): ?>
-            <tr>
-                <td><?= $row['id'] ?></td>
-                <td><?= $row['number'] ?></td>
-                <td><?= $row['date'] ?></td>
-                <td><?= $row['distance'] ?></td>
-                <td><?= $row['time'] ?></td>
-                <td><?= round($row['speed']) ?></td>
-                <td>
-                    <a href="?edit=<?= $row['id'] ?>">Taisyti</a>
-                    <a href="?delete=<?= $row['id'] ?>">Trinti</a>
-                </td>
-            </tr>
-        <?php endwhile; ?>
-    </table>
-    <?php
-} else {
-    echo 'nėra duomenų';
+if ($puslapis < 1){
+    $puslapis = 0;
 }
+
+if (isset($_GET['button']) && $_GET['button']=='auto' ){
+    $sql = "SELECT count(*) as viso FROM radars GROUP BY `number`";
+    $rezult = $conn->query($sql);
+$visoIrasu = $rezult->num_rows;
+    $sql = 'SELECT number, COUNT(*) AS kiekis, MAX(distance/time) AS maxgreitis FROM radars GROUP BY number ORDER BY maxgreitis DESC LIMIT '.($ip*$puslapis).', '.$ip;
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        ?>
+        <table>
+            <tr>
+                <th>Numeris</th>
+                <th>Irasu skaicius</th>
+                <th>Maksimalus greitis</th>
+            </tr>
+
+            <?php while($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?= $row['number'] ?></td>
+                    <td><?= $row['kiekis'] ?></td>
+                    <td><?= round($row['maxgreitis']) ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+        <?php
+        puslapiavimas($puslapis, $visoIrasu, $ip);
+    } else {
+        echo 'nėra duomenų';
+    }
+}
+
+        if (isset($_GET['button']) && $_GET['button']=='year' ){
+    $sql = "SELECT count(1) as viso FROM radars GROUP BY YEAR(date)";
+    $rezult = $conn->query($sql);
+            $visoIrasu = $rezult->num_rows;
+
+    $sql = 'SELECT date, COUNT(*) AS kiekis, 
+YEAR(date) AS metai, 
+MAX(distance/time) AS maxgreitis, 
+Min(distance/time) AS mingreitis, 
+AVG(distance/time) AS vidgreitis FROM radars GROUP BY metai ORDER BY maxgreitis DESC LIMIT '.($ip*$puslapis).', '.$ip;
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        ?>
+        <table>
+            <tr>
+                <th>Metai</th>
+                <th>Irasu skaicius</th>
+                <th>Maksimalus greitis</th>
+                <th>Minimalus greitis</th>
+                <th>Vidutinis greitis</th>
+            </tr>
+
+            <?php while($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?= $row['metai'] ?></td>
+                    <td><?= $row['kiekis'] ?></td>
+                    <td><?= round($row['maxgreitis']) ?></td>
+                    <td><?= round($row['mingreitis']) ?></td>
+                    <td><?= round($row['vidgreitis']) ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+        <?php
+        puslapiavimas($puslapis, $visoIrasu, $ip);
+    } else {
+        echo 'nėra duomenų';
+    }
+}
+
+        if (isset($_GET['button']) && $_GET['button']=='month' ){
+    $sql = "SELECT count(1) as viso FROM radars GROUP BY YEAR(date), MONTH(date)";
+    $rezult = $conn->query($sql);
+            $visoIrasu = $rezult->num_rows;
+
+    $sql = 'SELECT date, COUNT(*) AS kiekis, 
+YEAR(date) AS metai, 
+MONTH(date) AS menuo, 
+MAX(distance/time) AS maxgreitis, 
+Min(distance/time) AS mingreitis, 
+AVG(distance/time) AS vidgreitis FROM radars GROUP BY metai, menuo ORDER BY metai, menuo LIMIT '.($ip*$puslapis).', '.$ip;
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        ?>
+        <table>
+            <tr>
+                <th>Metai</th>
+                <th>Menuo</th>
+                <th>Irasu skaicius</th>
+                <th>Maksimalus greitis</th>
+                <th>Minimalus greitis</th>
+                <th>Vidutinis greitis</th>
+            </tr>
+
+            <?php while($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?= $row['metai'] ?></td>
+                    <td><?= $row['menuo'] ?></td>
+                    <td><?= $row['kiekis'] ?></td>
+                    <td><?= round($row['maxgreitis']) ?></td>
+                    <td><?= round($row['mingreitis']) ?></td>
+                    <td><?= round($row['vidgreitis']) ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+        <?php
+        puslapiavimas($puslapis, $visoIrasu, $ip);
+    } else {
+        echo 'nėra duomenų';
+    }
+}
+
+// išvedame automobilius
+
+ if (!isset($_GET['button'])){
+    $sql = "SELECT count(1) as viso FROM radars";
+    $rezult = $conn->query($sql);
+     $visoIrasu = $rezult->num_rows;
+
+    $sql = 'SELECT *, `distance`/`time`*3.6 as `speed` FROM radars ORDER BY number, date LIMIT '.($ip*$puslapis).', '.$ip;
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        ?>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Numeris</th>
+                <th>Data</th>
+                <th>Atstumas (km)</th>
+                <th>Laikas (h)</th>
+                <th>Greitis (km/h)</th>
+            </tr>
+
+            <?php while($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['number'] ?></td>
+                    <td><?= $row['date'] ?></td>
+                    <td><?= $row['distance'] ?></td>
+                    <td><?= $row['time'] ?></td>
+                    <td><?= round($row['speed']) ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+        <?php
+        puslapiavimas($puslapis, $visoIrasu, $ip);
+    } else {
+        echo 'nėra duomenų';
+    }
+}
+
 ?>
 </body>
 </html>
